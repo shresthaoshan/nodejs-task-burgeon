6396003942ed38ad6110310d977fb69597c58881
shresthaoshan <os02411418@student.ku.edu.np>
2020-09-19 00:07:37 +0545

    some refactorings and exception handling

--- server.js
+++ server.js
@@ -23,7 +23,14 @@ server.post('/register', async (req, res) => {
     if (!name || !email || !password) return res.status(400).send("All these fields are required: name, email, password.")
 
     // check if user already exists
-    let usersFromDB = await User.find({ email }).exec()
+
+    let usersFromDB = []
+    try {
+        usersFromDB = await User.find({ email }).exec()
+    } catch (e) {
+        console.log(e)
+        return res.status(500).send("There was a problem executing your request. Please, try again later.")
+    }
     if (usersFromDB.length > 0) return res.status(409).send("User with specified email already exists.")
 
     // validate data
@@ -35,7 +42,12 @@ server.post('/register', async (req, res) => {
     
     // proceed to store user info
     let encryptedPassword = encrypt(password, CRYPTO)
-    await (new User({ name, email, password: encryptedPassword})).save()
+    try {
+        await (new User({ name, email, password: encryptedPassword})).save()
+    } catch (e) {
+        console.log(e)
+        return res.status(500).send("There was a problem executing your request. Please, try again later.")
+    }
 
     // respond client
     res.send("Okay")
@@ -44,7 +56,13 @@ server.post('/login', async ({ body: { email = null, password = null } }, res) =
     if (!email || !password) return res.status(400).send("Both of these fields are required: email, password.")
 
     // attempt accessing user record from database
-    let userFromDB = await User.findOne({ email })
+    let userFromDB = null
+    try {
+        userFromDB = await User.findOne({ email })
+    } catch (e) {
+        console.log(e)
+        return res.status(500).send("There was a problem executing your request. Please, try again later.")
+    }
     if (!userFromDB) return res.status(404).send("User with specified email not found.")
 
     // attempt matching passwords
@@ -53,7 +71,7 @@ server.post('/login', async ({ body: { email = null, password = null } }, res) =
     
     // sign a token, set header and respond client
     let signature = sign({ id: userFromDB._id, email }, SERIALIZER)
-    res.header("Authorization", `Bearer ${signature}`).status(200).send("logged in")
+    res.status(200).send({ token: signature })
 })
 
 // todos
@@ -61,21 +79,38 @@ server.post('/todo', authenticate, async ({ user: { id }, body: { todo = "" } },
     if (!todo) return res.status(400).send("Todo is required.")
 
     // store todo as a new record
-    await (new Todo({ user: id, todo })).save()
+    try {
+        await (new Todo({ user: id, todo })).save()
+    } catch (e) {
+        console.log(e)
+        return res.status(500).send("There was a problem executing your request. Please, try again later.")
+    }
 
     // respond client
     res.status(200).json({ todo })
 })
 server.get('/todo', authenticate, async ({ user: { id } }, res) => {
     // access all todo records in reference to the user
-    let todos = await Todo.find({ user: id })
+    let todos = []
+    try {
+        todos = await Todo.find({ user: id })
+    } catch (e) {
+        console.log(e)
+        return res.status(500).send("There was a problem executing your request. Please, try again later.")
+    }
 
     // respond client w/ todos
     res.json({ todos })
 })
 server.delete('/todo/:todo_id', authenticate, async ({ params: { todo_id } }, res) => {
     // single-out and delete the todo record
-    let deletedTodo = await Todo.findByIdAndDelete(todo_id)
+    let deletedTodo = null
+    try {
+        deletedTodo = await Todo.findByIdAndDelete(todo_id)
+    } catch (e) {
+        console.log(e)
+        return res.status(500).send("There was a problem executing your request. Please, try again later.")
+    }
 
     // respond client
     if (!deletedTodo) return res.status(404).send("Todo record with specified id not found.")
