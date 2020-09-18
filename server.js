22b00fe8edcd24a9a76a8da19d8824b199ecaf30
shresthaoshan <os02411418@student.ku.edu.np>
2020-09-18 12:05:23 +0545

    todo handling done

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 const express = require('express'),
     server = express(),
     { User, Todo } = require('./models'),
-    { encrypt, sign } = require('./utils/crypto')
+    { encrypt, sign } = require('./utils/crypto'),
+    { authenticate } = require('./utils/middlewares')
 
 // middlewares
 server.use(express.json())
@@ -15,10 +16,8 @@ const {
     CRYPTO
 } = process.env
 
-// temporary data
-let users = []
-
 // routes
+// users
 server.post('/register', async (req, res) => {
     let { name = null, email = null, password = null } = req.body
     if (!name || !email || !password) return res.status(400).send("All these fields are required: name, email, password.")
@@ -41,18 +40,47 @@ server.post('/register', async (req, res) => {
     // respond client
     res.send("Okay")
 })
-server.post('/login', async (req, res) => {
-    let { email = null, password = null } = req.body
+server.post('/login', async ({ body: { email = null, password = null } }, res) => {
     if (!email || !password) return res.status(400).send("Both of these fields are required: email, password.")
 
+    // attempt accessing user record from database
     let userFromDB = await User.findOne({ email })
     if (!userFromDB) return res.status(404).send("User with specified email not found.")
+
+    // attempt matching passwords
     let encryptedPassword = encrypt(password, CRYPTO)
     if (encryptedPassword !== userFromDB.password) return res.status(401).send("Password did not match.")
-    // update till here
-    let signature = sign({ email, password: encryptedPassword }, SERIALIZER)
-    res.header("Authorization", `Bearer ${signature}`)
-    res.status(200).send("logged in")
+    
+    // sign a token, set header and respond client
+    let signature = sign({ id: userFromDB._id, email }, SERIALIZER)
+    res.header("Authorization", `Bearer ${signature}`).status(200).send("logged in")
 })
 
+// todos
+server.post('/todo', authenticate, async ({ user: { id }, body: { todo = "" } }, res) => {
+    if (!todo) return res.status(400).send("Todo is required.")
+
+    // store todo as a new record
+    await (new Todo({ user: id, todo })).save()
+
+    // respond client
+    res.status(200).json({ todo })
+})
+server.get('/todo', authenticate, async ({ user: { id } }, res) => {
+    // access all todo records in reference to the user
+    let todos = await Todo.find({ user: id })
+
+    // respond client w/ todos
+    res.json({ todos })
+})
+server.delete('/todo/:todo_id', authenticate, async ({ params: { todo_id } }, res) => {
+    // single-out and delete the todo record
+    let deletedTodo = await Todo.findByIdAndDelete(todo_id)
+
+    // respond client
+    if (!deletedTodo) return res.status(404).send("Todo record with specified id not found.")
+    res.status(200).send("deleted")
+})
+
+// initiate server
 server.listen(PORT, () => console.log("Server active!!!"))
--- utils/crypto.js
+++ utils/crypto.js
@@ -2,5 +2,12 @@ const crypto = require('crypto'),
     jwt = require('jsonwebtoken')
 module.exports = {
     encrypt: (password, secret) => crypto.createHmac('sha256', secret).update(password).digest('hex'),
-    sign: (payload, secret) => jwt.sign(payload, secret)
+    sign: (payload, secret) => jwt.sign(payload, secret),
+    verify: (payload, secret) => {
+        let uData = {}
+        jwt.verify(payload, secret, (err, userData) => {
+            if (!err) uData = { ...userData }
+        })
+        return uData
+    }
 }
